fb49785459b932862a0599e450f0e25145ed0e6c
Mansour Cheyo <mcheyo.mc@gmail.com>
2019-12-01 14:46:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,10 +18,14 @@ def summon_captain_planet(planeteers)# code an argument here
   # Your code here
   planeteers.map { |call| call.capitalize + '!' }
 end
+summon_captain_planet(planeteer_calls)
 
 def long_planeteer_calls# code an argument here
   # Your code here
+  planeteer_calls.any? { |call| call.length > 4 }
+
 end
+long_planeteer_calls(planeteer_calls)
 
 def find_the_cheese# code an argument here
   # the array below is here to help
